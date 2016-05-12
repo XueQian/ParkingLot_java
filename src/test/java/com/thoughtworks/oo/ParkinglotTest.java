a4ce4a9cf6d346511c9a128365592a1ca8bef197
XueQian <xueqian0321@outlook.com>
2016-05-12 16:33:51 +0800

    refactor--extract valiable in ParkingLotTest

--- src/test/java/com/thoughtworks/oo/ParkinglotTest.java
+++ src/test/java/com/thoughtworks/oo/ParkinglotTest.java
@@ -1,5 +1,6 @@
 package com.thoughtworks.oo;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -9,21 +10,38 @@ import static org.junit.Assert.assertTrue;
 
 public class ParkingLotTest {
 
+    private int validCarNumber1;
+    private int validCarNumber2;
+    private int invalidCarNumber;
+    private Car car1;
+    private Car car2;
+    private int parkingLotSpace;
+
+    @Before
+    public void setUp() {
+        validCarNumber1 = 1111;
+        validCarNumber2 = 2222;
+        invalidCarNumber = 3333;
+        car1 = new Car(validCarNumber1);
+        car2 = new Car(validCarNumber2);
+        parkingLotSpace = 5;
+    }
+
     @Test
     public void shouldParkOneCarCorrectly() {
 
-        ParkingLot parkingLot = new ParkingLot(1);
+        ParkingLot parkingLot = new ParkingLot(parkingLotSpace);
 
-        assertTrue(parkingLot.park(new Car(1)));
+        assertTrue(parkingLot.park(car1));
     }
 
     @Test
     public void shouldParkTwoCarsCorrectly() {
 
-        ParkingLot parkingLot = new ParkingLot(2);
+        ParkingLot parkingLot = new ParkingLot(parkingLotSpace);
 
-        assertTrue(parkingLot.park(new Car(1)));
-        assertTrue(parkingLot.park(new Car(2)));
+        assertTrue(parkingLot.park(car1));
+        assertTrue(parkingLot.park(car2));
     }
 
     @Test
@@ -31,37 +49,33 @@ public class ParkingLotTest {
 
         ParkingLot parkingLot = new ParkingLot(0);
 
-        assertFalse(parkingLot.park(new Car(1)));
+        assertFalse(parkingLot.park(car1));
     }
 
     @Test
     public void shouldNotParkOneCarWhenTheCarExistInParkinglot() {
 
-        ParkingLot parkingLot = new ParkingLot(2);
+        ParkingLot parkingLot = new ParkingLot(parkingLotSpace);
 
-        assertTrue(parkingLot.park(new Car(1)));
-        assertFalse(parkingLot.park(new Car(1)));
+        assertTrue(parkingLot.park(car1));
+        assertFalse(parkingLot.park(car1));
     }
 
     @Test
     public void shouldUnParkOneValidCarCorrectly() {
 
-        ParkingLot parkingLot = new ParkingLot(1);
+        ParkingLot parkingLot = new ParkingLot(parkingLotSpace);
+        parkingLot.park(car1);
 
-        Car car = new Car(1);
-        parkingLot.park(car);
-
-        assertThat(parkingLot.unPark(1), is(car));
+        assertThat(parkingLot.unPark(validCarNumber1), is(car1));
     }
 
     @Test
     public void shouldNotUnParkOneInvalidCar() {
 
-        ParkingLot parkingLot = new ParkingLot(1);
+        ParkingLot parkingLot = new ParkingLot(parkingLotSpace);
+        parkingLot.park(car1);
 
-        Car car = new Car(1);
-        parkingLot.park(car);
-
-        assertThat(parkingLot.unPark(2), is((Car) null));
+        assertThat(parkingLot.unPark(invalidCarNumber), is((Car) null));
     }
 }
